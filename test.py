8869b83fe793630187a8e0d712fabc4a596bad81
Paul J. Kreymborg <pk5192@princeton.edu>
2024-03-20 12:35:36 -0400

    added test command

--- test.py
+++ test.py
@@ -14,11 +14,11 @@ def setup_board():
     GPIO.setmode(GPIO.board)
 
     # Set up pins we are going to be using
-    GPIO.setup(LDAC_1,GPIO.out)
-    GPIO.setup(LDAC_2,GPIO.out)
+    GPIO.setup(LDAC_1,GPIO.OUT)
+    GPIO.setup(LDAC_2,GPIO.OUT)
 
-    GPIO.setup(SDA, GPIO.out)
-    GPIO.setup(SCL, GPIO.out)
+    GPIO.setup(SDA, GPIO.OUT)
+    GPIO.setup(SCL, GPIO.OUT)
 
     GPIO.output(LDAC_1, GPIO.HIGH)
     GPIO.output(LDAC_2, GPIO.HIGH)
@@ -34,12 +34,74 @@ def do_start_condition():
     # Pull SCL low, so ready for first bit
     GPIO.output(SCL, GPIO.LOW)
 
-def send_byte():
-    GPIO.output(SCL)
+def num_to_byte_str(val):
+    return '{0:b}'.format(val).zfill(8)
+
+def send_byte(val):
+    if val > 0xFF or val < 0 or int(val) != val:
+        raise Exception("Invalid byte ''".format(val))
+    
+    # Take control of SDA
+    GPIO.setup(SDA, GPIO.OUT)
+
+    # Set SCL and SDA low to start out
+    # GPIO.output(SCL, GPIO.LOW)
+    GPIO.output(SDA, GPIO.LOW)
+    dat_high = False
+
+    for digit in num_to_byte_str:
+        if digit == "1":
+            if not dat_high:
+                dat_high = True
+                GPIO.output(SDA, GPIO.HIGH)
+            else:
+                # Don't need to change data bit
+                pass
+        else:
+            if dat_high:
+                dat_high = False
+                GPIO.output(SDA, GPIO.LOW)
+            else:
+                # Don't need to change data bit
+                pass
+        GPIO.output(SCL, GPIO.HIGH)
+        GPIO.output(SCL, GPIO.LOW)
+    
+    # Release SDA
+    GPIO.setup(SDA, GPIO.OUT)
+
+    # Check acknowledge
+    GPIO.output(SCL, GPIO.HIGH)
+    ackbit_high = GPIO.input(SDA)
+    GPIO.output(SCL, GPIO.LOW)
+
+    # Low bit indicates acknowledgement
+    return not ackbit_high
+
+
+def main():
+    # print(MCP_DEF_ADDR)
+    # Binary address as string
+    # bin_addr_str = '{0:b}'.format(MCP_DEF_ADDR).zfill(8)
+    # print(bin_addr_str)
+    mcp_7bit_address = '{0:b}'.format(MCP_DEF_ADDR).zfill(7)
+    # add a R/W 0 bit (read)
+    read_mcp_byte = mcp_7bit_address + "0"
+
+
+    do_start_condition()
+    general_call_command = '00000000'
+    if not send_byte(general_call_command):
+        raise Exception("Failed")
+    
+    general_call_reset_byte = '00000110'
+    if not send_byte(general_call_reset_byte):
+        raise Exception("Failed")
 
 
 if __name__ == "__main__":
-    print(MCP_DEF_ADDR)
-    # Binary address as string
-    bin_addr_str = '{0:b}'.format(MCP_DEF_ADDR).zfill(8)
-    print(bin_addr_str)
+    try:
+        main()
+    except:
+        GPIO.cleanup()
+        raise
