18fc7dd0711148b8914f4a2b219ee6e5ea26d933
Paul J. Kreymborg <pk5192@princeton.edu>
2024-03-20 13:45:08 -0400

    remove sleep

--- test.py
+++ test.py
@@ -104,7 +104,7 @@ def send_byte(
             raise Exception("Must provide GPIO HIGH/LOW ldac bit")
         GPIO.output(LDAC_2, set_ldac2_before_ack)
     
-    time.sleep(10/2000000)
+    # time.sleep(10/2000000)
     # Check acknowledge
     GPIO.output(SCL, GPIO.HIGH)
     ackbit_high = GPIO.input(SDA)
